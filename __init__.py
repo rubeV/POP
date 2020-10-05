3c86c8ddacf187a9a10aaede50943401d41df6e3
rubeV <54709444+rubeV@users.noreply.github.com>
2020-10-05 18:10:59 +0100

    Blueprint for authentication of registration and logging in

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 
 def create_app(test_config=None):
-    # create and configure the app
+    # create and configure the app, factory function
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
@@ -30,6 +30,8 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
 
